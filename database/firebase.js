f74df1b5925750f43ecd74dc568ff40a00279b5a
KAROUI-FARES <126524113+KAROUIFARES@users.noreply.github.com>
2023-11-08 16:01:36 +0100

    Update firebase.js

--- database/firebase.js
+++ database/firebase.js
@@ -2,14 +2,14 @@ import firebase from "firebase/compat/app"
 import 'firebase/compat/auth'
 import 'firebase/compat/firestore'
 const firebaseConfig = {
-    apiKey: "AIzaSyApB3lBM5JuBSYPqE1-buTUO3CBJVkO58c",
-    authDomain: "fir-auth-f9d62.firebaseapp.com",
-    databaseURL: "https://fir-auth-f9d62-default-rtdb.firebaseio.com",
-    projectId: "fir-auth-f9d62",
-    storageBucket: "fir-auth-f9d62.appspot.com",
-    messagingSenderId: "510927265017",
-    appId: "1:510927265017:web:06837561e491539a809628",
-    measurementId: "G-TPL7F20F5W"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 firebase.initializeApp(firebaseConfig);
 export default firebase;
